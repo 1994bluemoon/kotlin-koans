c8374a905ffaae08378cfaf482a209963947a629
Henry Duong <minhhoang94_bluemoon@yahoo.com.vn>
2018-04-09 12:03:35 +0700

    task8,9,10

--- src/i_introduction/_10_Object_Expressions/n10ObjectExpressions.kt
+++ src/i_introduction/_10_Object_Expressions/n10ObjectExpressions.kt
@@ -2,7 +2,6 @@ package i_introduction._10_Object_Expressions
 
 import util.TODO
 import util.doc10
-import java.util.*
 
 fun todoTask10(): Nothing = TODO(
     """
@@ -18,6 +17,6 @@ fun todoTask10(): Nothing = TODO(
 
 fun task10(): List<Int> {
     val arrayList = arrayListOf(1, 5, 2)
-    Collections.sort(arrayList, todoTask10())
+    arrayList.sortDescending()
     return arrayList
 }
