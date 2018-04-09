89bde0c91c037ce7b9c448e3c2313857cbcafb7a
Henry Duong <minhhoang94_bluemoon@yahoo.com.vn>
2018-04-09 17:27:06 +0700

    task 16,17,18,19

--- src/ii_collections/n16FlatMap.kt
+++ src/ii_collections/n16FlatMap.kt
@@ -1,5 +1,7 @@
 package ii_collections
 
+import v_builders.data.getProducts
+
 fun example() {
 
     val result = listOf("abc", "12").flatMap { it.toList() }
@@ -7,12 +9,39 @@ fun example() {
     result == listOf('a', 'b', 'c', '1', '2')
 }
 
-val Customer.orderedProducts: Set<Product> get() {
+val Customer.orderedProducts: Set<Product> get() = this.orders.flatMap { it.products }.toSet()
+
+/*{
+    var products: ArrayList<Product> = ArrayList()
+    for (order in this.orders){
+        for (product in order.products){
+            products.add(product)
+        }
+    }
+    return products.toSet()
+}*/
+
+/*{
     // Return all products this customer has ordered
     todoCollectionTask()
 }
+*/
+val Shop.allOrderedProducts: Set<Product> get() = this.customers.flatMap { it.orders }.flatMap { it.products }.toSet()
 
-val Shop.allOrderedProducts: Set<Product> get() {
+/*{
+    var product: ArrayList<Product> = ArrayList()
+    for (cus in this.customers){
+        for (pro in cus.orderedProducts){
+            if (!product.contains(pro)){
+                product.add(pro)
+            }
+        }
+    }
+    return product.toSet()
+}*/
+
+/*{
     // Return all products that were ordered by at least one customer
     todoCollectionTask()
 }
+*/
--- src/ii_collections/n17MaxMin.kt
+++ src/ii_collections/n17MaxMin.kt
@@ -1,16 +1,44 @@
 package ii_collections
 
+import java.time.OffsetDateTime
+
 fun example4() {
     val max = listOf(1, 42, 4).max()
     val longestString = listOf("a", "b").maxBy { it.length }
 }
 
 fun Shop.getCustomerWithMaximumNumberOfOrders(): Customer? {
+    var array: ArrayList<Int> = ArrayList()
+    for (cus in this.customers){
+        array.add(cus.orders.count())
+    }
+    var max = array.max()
+    var pos = 0
+    for (i in array){
+        if (max == array[i])
+            pos =i
+    }
+    return this.customers.get(pos)
+}
+/*{
     // Return a customer whose order count is the highest among all customers
     todoCollectionTask()
-}
+}*/
 
 fun Customer.getMostExpensiveOrderedProduct(): Product? {
     // Return the most expensive product which has been ordered
-    todoCollectionTask()
+    //todoCollectionTask()
+    var price: ArrayList<Double> = ArrayList()
+    var maxPrice: Double = 0.0
+    for (product in this.orderedProducts){
+        price.add(product.price)
+    }
+    maxPrice = price.max()!!
+    var pos = 0
+    for (i in 0..price.size - 1){
+        if (maxPrice == price[i])
+            pos = i
+    }
+    return this.orderedProducts.toList().get(pos)
+
 }
