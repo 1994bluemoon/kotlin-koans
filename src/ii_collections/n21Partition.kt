5f189375d32845f9d390c2656f14c97b7e1d7708
Henry Duong <minhhoang94_bluemoon@yahoo.com.vn>
2018-04-10 23:02:09 +0700

    task 21,22,23,24

--- src/ii_collections/n21Partition.kt
+++ src/ii_collections/n21Partition.kt
@@ -12,5 +12,6 @@ fun example8() {
 
 fun Shop.getCustomersWithMoreUndeliveredOrdersThanDelivered(): Set<Customer> {
     // Return customers who have more undelivered orders than delivered
-    todoCollectionTask()
+    //todoCollectionTask()
+    return customers.filter { (it.orders.filter { it.isDelivered == false }.size - it.orders.filter { it.isDelivered == true }.size) > 0 }.toSet()
 }
--- src/ii_collections/n23CompoundTasks.kt
+++ src/ii_collections/n23CompoundTasks.kt
@@ -1,18 +1,23 @@
 package ii_collections
 
-fun Shop.getCustomersWhoOrderedProduct(product: Product): Set<Customer> {
+fun Shop.getCustomersWhoOrderedProduct(product: Product): Set<Customer> = this.customers.filter { it.orders.flatMap { it.products }.contains(product) }.toSet()
+
+/*{
     // Return the set of customers who ordered the specified product
     todoCollectionTask()
-}
+}*/
 
 fun Customer.getMostExpensiveDeliveredProduct(): Product? {
     // Return the most expensive product among all delivered products
     // (use the Order.isDelivered flag)
-    todoCollectionTask()
+    return this.orders.filter { it.isDelivered == true }.flatMap { it.products }.maxBy { it.price }
+    //todoCollectionTask()
 }
 
 fun Shop.getNumberOfTimesProductWasOrdered(product: Product): Int {
     // Return the number of times the given product was ordered.
     // Note: a customer may order the same product for several times.
-    todoCollectionTask()
+
+    return customers.filter { it.orders.flatMap { it.products }.contains(product) }.flatMap { it.orders }.flatMap { it.products }.count { it == product }
+    //todoCollectionTask()
 }
